3dc89a89f02ef4a757aa2c434fd7af57c139b766
fallgelb22061940 <67679788+fallgelb22061940@users.noreply.github.com>
2024-05-28 23:30:55 +0200

    test

--- projekat/main.c
+++ projekat/main.c
@@ -1,5 +1,12 @@
 #include <msp430.h> 
-
+/*
+ * Napisati program kojim se vrši akvizicija signala sa jednog naponskog kanala. Na komandu
+pritiska tastera startuje se akvizicija sa uèestanošæu od 20 sample/s, i sekvenca dužina 200 odbiraka
+se smešta u ram memoriju kontrolera. U toku akvizicije ukljuèena je LE dioda. Po završetku
+akvizicije na komandu pritiska preostalih tastera se na LED displej ispisuje minimalna, srednja i
+maksimalna vrednost uzorkovanog signala, u zavisnosti od toga koji je od preostalih tastera
+pritisnut.
+ */
 
 /**
  * main.c
