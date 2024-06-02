78b8e9a6dd552f6701b8731aa03a63489c2638ac
fallgelb22061940 <67679788+fallgelb22061940@users.noreply.github.com>
2024-06-02 21:23:20 +0200

    push

--- projekat/main.c
+++ projekat/main.c
@@ -14,6 +14,7 @@ pritisnut.
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	//buttons
     // configure button S1
     P1REN |= BIT4;              // enable pull up/down
     P1OUT |= BIT4;              // set pull up
@@ -21,10 +22,43 @@ int main(void)
     P1IES |= BIT4;              // interrupt on falling edge
     P1IFG &= ~BIT4;             // clear flag
     P1IE  |= BIT4;              // enable interrupt
-    /* initialize Timer A */
-    TA0CCR0 = TIMER_PERIOD;     // debounce period
+    // configure button S2
+    P1REN |= BIT5;              // enable pull up/down
+    P1OUT |= BIT5;              // set pull up
+    P1DIR &= ~BIT5;             // configure P1.4 as in
+    P1IES |= BIT5;              // interrupt on falling edge
+    P1IFG &= ~BIT5;             // clear flag
+    P1IE  |= BIT5;              // enable interrupt
+    // configure button S3
+    P1REN |= BIT1;              // enable pull up/down
+    P1OUT |= BIT1;              // set pull up
+    P1DIR &= ~BIT1;             // configure P1.4 as in
+    P1IES |= BIT1;              // interrupt on falling edge
+    P1IFG &= ~BIT1;             // clear flag
+    P1IE  |= BIT1;              // enable interrupt
+    // configure button S4
+    P2REN |= BIT1;              // enable pull up/down
+    P2OUT |= BIT1;              // set pull up
+    P2DIR &= ~BIT1;             // configure P1.4 as in
+    P2IES |= BIT1;              // interrupt on falling edge
+    P2IFG &= ~BIT1;             // clear flag
+    P2IE  |= BIT1;              // enable interrupt
+    // initialize Timer A
+    TA0CCR0 = 32768 / 20 - 1;;// f_OUT = 2 Hz, f_ACLK = 32768 Hz ID=8 => T_OUT = 2048.
     TA0CCTL0 = CCIE;            // enable CCR0 interrupt
     TA0CTL = TASSEL__ACLK;
+    //7seg
+    P7DIR |= BIT0;              // set P7.0 as out (SEL1)
+    P7OUT &= ~BIT0;             // enable display 1
+    // a,b,c,d,e,f,g
+    P2DIR |= BIT6 | BIT3;       // configure P2.3 and P2.6 as out
+    P3DIR |= BIT7;              // configure P3.7 as out
+    P4DIR |= BIT3 | BIT0;       // configure P4.0 and P4.3 as out
+    P8DIR |= BIT2 | BIT1;       // configure P8.1 and P8.2 as out
+    //ADC
+    P6SEL |= BIT1;              // enable left pot
+    ADC12CTL0 &= ~ADC12ENC;     // disable ADC before configuring
+    ADC12CTL0 |= ADC12SHT0_3 + ADC12ON; //32 cycles for sampling, ADC ON
 
 	return 0;
 }
