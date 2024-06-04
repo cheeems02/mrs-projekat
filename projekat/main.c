e41316f0e8bdbbdd289383391b63dc2aaaa7ac1b
fallgelb22061940 <67679788+fallgelb22061940@users.noreply.github.com>
2024-06-04 20:33:49 +0200

    trial

--- projekat/main.c
+++ projekat/main.c
@@ -17,6 +17,7 @@
 #define MUX_PERIOD     (164)  // ~5ms
 #define SECOND 32768            //1s
 #define DEBOUNCE 33             //1ms
+#define DISPLAY_START 69
 volatile unsigned int samples[NUM_SAMPLES] = { 0 };
 volatile unsigned int sample_index = 0;
 volatile unsigned int sampling = 0;
@@ -25,33 +26,33 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
     //buttons
     // configure button S1 - start
-    P1REN |= BIT4;              // enable pull up/down
-    P1OUT |= BIT4;              // set pull up
-    P1DIR &= ~BIT4;             // configure P1.4 as in
-    P1IES |= BIT4;              // interrupt on falling edge
-    P1IFG &= ~BIT4;             // clear flag
-    P1IE |= BIT4;               // enable interrupt
-    // configure button S2 - min
-    P1REN |= BIT5;              // enable pull up/down
-    P1OUT |= BIT5;              // set pull up
-    P1DIR &= ~BIT5;             // configure P1.4 as in
-    P1IES |= BIT5;              // interrupt on falling edge
-    P1IFG &= ~BIT5;             // clear flag
-    P1IE |= BIT5;               // enable interrupt
-    // configure button S3 -  avg
-    P1REN |= BIT1;              // enable pull up/down
-    P1OUT |= BIT1;              // set pull up
-    P1DIR &= ~BIT1;             // configure P1.4 as in
-    P1IES |= BIT1;              // interrupt on falling edge
-    P1IFG &= ~BIT1;             // clear flag
-    P1IE |= BIT1;               // enable interrupt
-    // configure button S4 - max
     P2REN |= BIT1;              // enable pull up/down
     P2OUT |= BIT1;              // set pull up
     P2DIR &= ~BIT1;             // configure P1.4 as in
     P2IES |= BIT1;              // interrupt on falling edge
     P2IFG &= ~BIT1;             // clear flag
     P2IE |= BIT1;               // enable interrupt
+    // configure button S2 -  avg
+    P1REN |= BIT1;              // enable pull up/down
+    P1OUT |= BIT1;              // set pull up
+    P1DIR &= ~BIT1;             // configure P1.4 as in
+    P1IES |= BIT1;              // interrupt on falling edge
+    P1IFG &= ~BIT1;             // clear flag
+    P1IE |= BIT1;               // enable interrupt
+    // configure button S3 - max
+    P1REN |= BIT4;              // enable pull up/down
+    P1OUT |= BIT4;              // set pull up
+    P1DIR &= ~BIT4;             // configure P1.4 as in
+    P1IES |= BIT4;              // interrupt on falling edge
+    P1IFG &= ~BIT4;             // clear flag
+    P1IE |= BIT4;               // enable interrupt
+    // configure button S4 - min
+    P1REN |= BIT5;              // enable pull up/down
+    P1OUT |= BIT5;              // set pull up
+    P1DIR &= ~BIT5;             // configure P1.4 as in
+    P1IES |= BIT5;              // interrupt on falling edge
+    P1IFG &= ~BIT5;             // clear flag
+    P1IE |= BIT5;               // enable interrupt
     // led
     P1DIR |= BIT0;              //set P1.0 as output
     P1OUT &= ~BIT0;             //set output as 0
@@ -94,59 +95,47 @@ int main(void)
     }
     return 0;
 }
+void __attribute__ ((interrupt(PORT1_VECTOR))) P1ISR(void) //debounce
+{
+    if ((P1IFG & BIT4) != 0)        // check if P1.4 flag is set
+    {
+        /* start timer */
+        TA0CTL |= MC__UP;
+        P1IFG &= ~BIT4;             // clear P1.4 flag
+        P1IE &= ~BIT4;              // disable P1.4 interrupt
+    }
+    if ((P1IFG & BIT1) != 0)        // check if P1.4 flag is set
+    {
+        /* start timer */
+        TA0CTL |= MC__UP;
+        P1IFG &= ~BIT1;             // clear P1.4 flag
+        P1IE &= ~BIT1;              // disable P1.4 interrupt
+    }
+    if ((P1IFG & BIT5) != 0)        // check if P1.4 flag is set
+    {
+        /* start timer */
+        TA0CTL |= MC__UP;
+        P1IFG &= ~BIT5;             // clear P1.4 flag
+        P1IE &= ~BIT5;              // disable P1.4 interrupt
+    }
+}
+void __attribute__ ((interrupt(PORT2_VECTOR))) P2ISR(void) //debounce, start
+{
+    if ((P2IFG & BIT1) != 0)        // check if P1.4 flag is set
+    {
+        /* start timer */
+        TA0CTL |= MC__UP;
+        P2IFG &= ~BIT1;             // clear P1.4 flag
+        P2IE &= ~BIT1;              // disable P1.4 interrupt
+    }
+}
 static volatile uint8_t cifre[2] = { 0 };
 void display(const uint8_t broj)
 {                               //0x0F je za skidanje viših cifara
     cifre[1] = (broj >> 4) & 0x0F;
     cifre[0] = broj & 0x0F;
 }
-void start()
-{
-    sampling = 1;
-    ADC12CTL0 |= ADC12ENC;      // Enable conversion
-    P1OUT |= BIT0;              //set output as 1
-}
-void stop()
-{
-    sampling = 0;
-    P1OUT &= ~BIT0;  // LED OFF
-    ADC12CTL0 &= ~ADC12ENC; //disable ADC
-}
-void minimum()
-{
-    unsigned int i = 0;
-    unsigned int minVal = 0xFFFF;
-    for (i = 0; i < 200; i++)
-    {
-        if (samples[i] < minVal)
-        {
-            minVal = samples[i];
-        }
-    }
-}
-void average()
-{
-    unsigned int i = 0;
-    unsigned long avg = 0;
-    for (i = 0; i < 200; i++)
-    {
-        avg += samples[i];
-    }
-    avg = avg / 200;
-}
-void maximum()
-{
-    unsigned int i = 0;
-    unsigned int maxVal = 0;
-    for (i = 0; i < 200; i++)
-    {
-        if (samples[i] > maxVal)
-        {
-            maxVal = samples[i];
-        }
-    }
-}
-void __attribute__ ((interrupt(TIMER1_A2_VECTOR))) TA1CCR0ISR(void)
+void __attribute__ ((interrupt(TIMER2_A0_VECTOR))) TA2CCR0ISR(void) //screen mux
 {
     static uint8_t current_digit = 0;
     /* algorithm:
@@ -168,4 +157,64 @@ void __attribute__ ((interrupt(TIMER1_A2_VECTOR))) TA1CCR0ISR(void)
     }
     current_digit = (current_digit + 1) & 0x01;
     return;
+}
+void __attribute__ ((interrupt(TIMER1_A0_VECTOR))) TA1CCR0ISR(void) //debounce
+{
+    if ((P2IN & BIT1) != 0)
+    {
+        sampling = 1;
+        ADC12CTL0 |= ADC12ENC;      // Enable conversion
+        P1OUT |= BIT0;              //set output as 1
+    }
+    if ((P1IN & BIT1) != 0)
+    {
+        unsigned int i = 0;
+        unsigned long avg = 0;
+        for (i = 0; i < 200; i++)
+        {
+            avg += samples[i];
+        }
+        avg = avg / 200;
+    }
+    if ((P1IN & BIT4) != 0)
+    {
+        unsigned int i = 0;
+        unsigned int maxVal = 0;
+        for (i = 0; i < 200; i++)
+        {
+            if (samples[i] > maxVal)
+            {
+                maxVal = samples[i];
+            }
+        }
+    }
+    if ((P1IN & BIT5) != 0)
+    {
+        unsigned int i = 0;
+        unsigned int minVal = 0xFFFF;
+        for (i = 0; i < 200; i++)
+        {
+            if (samples[i] < minVal)
+            {
+                minVal = samples[i];
+            }
+        }
+    }
+}
+void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) TA0CCR0ISR(void) //stop counting
+{
+    if (sampling == 1)
+    {
+        if (sample_index<200)
+        {
+            samples[sample_index]=ADC12MEM0;
+            TA0CTL |= MC__UP;
+        }
+        else
+        {
+            sampling = 0;
+            P1OUT &= ~BIT0;  // LED OFF
+            ADC12CTL0 &= ~ADC12ENC; //disable ADC
+        }
+    }
 }
